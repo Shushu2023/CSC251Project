36f4d99f9c103b00058a325fb77c7d5346351e61
snimeri <snimeri0@email.cpcc.edu>
2023-02-05 10:43:05 -0500

    Update Policy.java
    Added code for Policy class

---
+++ Policy.java
@@ -0,0 +1,118 @@
+public class Policy {
+	
+	private String policyNumber = null;
+	private String providerName= null;
+	private String policyHolderFirstName= null;
+	private String policyHolderLastName= null;
+	private int age= 0;
+	private String smokingStatus= null;
+	private double  policyHolderHeight= 0;
+	private double policyHolderWeight= 0;
+	
+	public Policy() {
+	
+	}
+
+	public Policy(String policyNumber, String providerName, String policyHolderFirstName, String policyHolderLastName,
+			int age, String smokingStatus, double policyHolderHeight, double policyHolderWeight) {
+		
+		this.policyNumber = policyNumber;
+		this.providerName = providerName;
+		this.policyHolderFirstName = policyHolderFirstName;
+		this.policyHolderLastName = policyHolderLastName;
+		this.age = age;
+		this.smokingStatus = smokingStatus;
+		this.policyHolderHeight = policyHolderHeight;
+		this.policyHolderWeight = policyHolderWeight;
+	}
+
+	public String getPolicyNumber() {
+		return policyNumber;
+	}
+
+	public void setPolicyNumber(String policyNumber) {
+		this.policyNumber = policyNumber;
+	}
+
+	public String getProviderName() {
+		return providerName;
+	}
+
+	public void setProviderName(String providerName) {
+		this.providerName = providerName;
+	}
+
+	public String getPolicyHolderFirstName() {
+		return policyHolderFirstName;
+	}
+
+	public void setPolicyHolderFirstName(String policyHolderFirstName) {
+		this.policyHolderFirstName = policyHolderFirstName;
+	}
+
+	public String getPolicyHolderLastName() {
+		return policyHolderLastName;
+	}
+
+	public void setPolicyHolderLastName(String policyHolderLastName) {
+		this.policyHolderLastName = policyHolderLastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getSmokingStatus() {
+		return smokingStatus;
+	}
+
+	public void setSmokingStatus(String smokingStatus) {
+		this.smokingStatus = smokingStatus;
+	}
+
+	public double getPolicyHolderHeight() {
+		return policyHolderHeight;
+	}
+
+	public void setPolicyHolderHeight(double  policyHolderHeight) {
+		this.policyHolderHeight = policyHolderHeight;
+	}
+
+	public double  getPolicyHolderWeight() {
+		return policyHolderWeight;
+	}
+
+	public void setPolicyHolderWeight(double policyHolderWeight) {
+		this.policyHolderWeight = policyHolderWeight;
+	}
+	public double calculateBMI() {
+		
+		double bmi = (this.policyHolderWeight*703)/Math.pow(this.policyHolderWeight,2);
+		return bmi;
+	}
+	public double calculateInsurancePolicyPrice() {
+		double price =0;
+		int baseFee = 600;
+		
+		if(this.age>50)
+		{	
+			price = baseFee+75;
+		}
+		
+		if(this.smokingStatus.equals("Smoker")) 
+		{
+			price = baseFee+100;
+		}
+		
+		if(this.calculateBMI()>35) {
+			price = baseFee + ((this.calculateBMI()-35)*20);
+		}
+		return price;
+	}
+	
+
+}
